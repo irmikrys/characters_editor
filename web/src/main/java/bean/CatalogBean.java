5056cc3379572f0673bc2fae558c7ea4bb99c5b7
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-06-11 10:47:50 +0200

    Displaying woods with elves in a tree structure.

--- web/src/main/java/bean/CatalogBean.java
+++ web/src/main/java/bean/CatalogBean.java
@@ -1,6 +1,10 @@
 package bean;
 
 import boundary.CharactersServiceRemote;
+import model.Elf;
+import model.Wood;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -8,6 +12,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
@@ -18,6 +23,7 @@ public class CatalogBean implements Serializable {
     private static final long serialVersionUID = -9217712787886869451L;
 
     private CharactersServiceRemote bean;
+    private TreeNode root;
 
     public CatalogBean() throws NamingException {
         final Properties jndiProperties = new Properties();
@@ -27,11 +33,27 @@ public class CatalogBean implements Serializable {
         bean = (CharactersServiceRemote) context.lookup(
                 "ejb:/web_main_war/CharactersService!boundary.CharactersServiceRemote"
         );
-        getWoods();
+        List<Wood> woods = getWoods();
+        root = new DefaultTreeNode("Woods", null);
+        woods.forEach(wood -> addNode(root, wood.getName(), wood.getElfByIdWood()));
         System.out.println("Catalog bean constructor end");
     }
 
-    public List getWoods() {
+    private void addNode(TreeNode parentNode, String nodeName, Collection<Elf> children) {
+        TreeNode node = new DefaultTreeNode(nodeName, parentNode);
+        node.setExpanded(true);
+        if (children != null) {
+            for (Elf e : children) {
+                addNode(node, e.getName(), null);
+            }
+        }
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public List<Wood> getWoods() {
         return new LinkedList<>(bean.getAllWoods());
     }
 }
