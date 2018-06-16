1d554a02e618e4de77b42249a3e9a22d1f827933
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-06-16 11:05:43 +0200

    Form for changing users passwords.

--- web/src/main/java/bean/UserBean.java
+++ web/src/main/java/bean/UserBean.java
@@ -19,6 +19,11 @@ public class UserBean implements Serializable {
     private static final long serialVersionUID = 3432068515031339999L;
 
     private List<User> users;
+    private String username;
+    private String newPassword;
+
+    private String successMessage;
+    private String errorMessage;
 
     private CharactersServiceRemote charactersServiceRemote;
 
@@ -30,6 +35,20 @@ public class UserBean implements Serializable {
     @PostConstruct
     public void init() {
         users = charactersServiceRemote.getAllUsers();
+        clearFields();
+    }
+
+    private void clearFields() {
+        username = null;
+        newPassword = null;
+        successMessage = null;
+        errorMessage = null;
+    }
+
+    public void submitPassword() {
+        System.out.format("User: %s, new password: %s\n", username, newPassword);
+        clearFields();
+        successMessage = "New password submitted!";
     }
 
     public List<User> getUsers() {
@@ -38,5 +57,37 @@ public class UserBean implements Serializable {
 
     public void setUsers(List<User> users) {
         this.users = users;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getSuccessMessage() {
+        return successMessage;
+    }
+
+    public void setSuccessMessage(String successMessage) {
+        this.successMessage = successMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 }
