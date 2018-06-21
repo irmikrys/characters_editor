f5b06dbb472d989b8e7b30a0726ba028a7d00657
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-06-21 12:47:29 +0200

    [REST] Adding element from client.

--- rest/src/main/java/app/ClientREST.java
+++ rest/src/main/java/app/ClientREST.java
@@ -1,9 +1,11 @@
 package app;
 
 import dto.CategoryDTO;
+import dto.ElementDTO;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -14,6 +16,7 @@ class ClientREST {
 
     private static final String REST_URI = "http://localhost:8080/rest_main_war/rest";
     private static final String MAIN_PATH = "editor/categories";
+    private static final String ELEMENTS_PATH = "/elements";
     private Client client = ClientBuilder.newClient();
 
     List<CategoryDTO> getAllCategories() {
@@ -38,5 +41,16 @@ class ClientREST {
         System.out.println("Get category by id status: " + response.getStatus());
         return Optional.ofNullable(response
                 .readEntity(CategoryDTO.class));
+    }
+
+    Integer addElement(Integer idCategory, ElementDTO elementDTO) {
+        Response response = client
+                .target(REST_URI)
+                .path(MAIN_PATH + "/" + idCategory.toString() + ELEMENTS_PATH)
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(elementDTO, MediaType.APPLICATION_JSON));
+        System.out.println("Add element status: " + response.getStatus());
+        return response
+                .getStatus();
     }
 }
--- rest/src/main/java/app/Main.java
+++ rest/src/main/java/app/Main.java
@@ -1,6 +1,7 @@
 package app;
 
 import dto.CategoryDTO;
+import dto.ElementDTO;
 
 import java.util.Optional;
 
@@ -16,11 +17,31 @@ public class Main {
                     categoryDTO.getElementDTOS().forEach(System.out::println);
                 });
 
-        Optional<CategoryDTO> category1 = clientREST.getCategoryById(3);
-        category1.ifPresent(System.out::println);
+        Optional<CategoryDTO> category1 = clientREST.getCategoryById(1);
+        category1.ifPresent(categoryDTO -> {
+                    System.out.println(categoryDTO);
+                    categoryDTO.getElementDTOS().forEach(System.out::println);
+                }
+        );
 
         Optional<CategoryDTO> category2 = clientREST.getCategoryById(9);
-        category2.ifPresent(System.out::println);
+        category2.ifPresent(categoryDTO -> {
+                    System.out.println(categoryDTO);
+                    categoryDTO.getElementDTOS().forEach(System.out::println);
+                }
+        );
+
+        clientREST.addElement(
+                1,
+                new ElementDTO("TestElemRest", 20, 3, 90)
+        );
+
+        Optional<CategoryDTO> category3 = clientREST.getCategoryById(1);
+        category3.ifPresent(categoryDTO -> {
+                    System.out.println(categoryDTO);
+                    categoryDTO.getElementDTOS().forEach(System.out::println);
+                }
+        );
 
     }
 }
