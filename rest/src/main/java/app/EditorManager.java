b7adc60a8b46036196e9f367704c457c0ffe524d
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-06-21 09:14:52 +0200

    [REST] Getting chosen category with elements.

--- rest/src/main/java/app/EditorManager.java
+++ rest/src/main/java/app/EditorManager.java
@@ -2,15 +2,18 @@ package app;
 
 import dao.CategoryDAO;
 import dto.CategoryDTO;
+import model.Category;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Stateless
@@ -20,7 +23,6 @@ public class EditorManager {
     @Inject
     private CategoryDAO categoryDAO;
 
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/categories")
@@ -29,8 +31,26 @@ public class EditorManager {
                 .stream()
                 .map(CategoryDTO::new)
                 .collect(Collectors.toList());
-        return Response.status(Response.Status.OK)
+        return Response
+                .status(Response.Status.OK)
                 .entity(categoryDTOS)
                 .build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/categories/{idCategory}")
+    public Response getCategoryById(@PathParam("idCategory") Integer idCategory) {
+        Optional<Category> category = categoryDAO.findById(idCategory);
+        if (category.isPresent()) {
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(new CategoryDTO(category.get()))
+                    .build();
+        } else {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .build();
+        }
     }
 }
